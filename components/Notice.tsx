7159e39f8d35a92ee62a079aef6e79cc688f7d16
karimscoding <rejaulkarim66666@gmail.com>
2023-10-15 04:41:14 +0800

    changed notice url

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -13,7 +13,10 @@ const Notice = () => {
   return showNotice ? (
     <div className="bg-gradient-to-r from-primary to-secondary text-white h-8 flex items-center justify-between px-4 md:px-10">
       <p className="text-center flex-1 text-sm hover:underline uppercase font-semibold tracking-wider">
-        <Link href="https://www.linkedin.com/in/rejaulkareem/" target="_blank">
+        <Link
+          href="https://www.upwork.com/freelancers/~01bde2fb752a090f52"
+          target="_blank"
+        >
           {" "}
           Build with 🖤 by Rejaul.dev
         </Link>
