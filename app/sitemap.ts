0216a514cf2edb9638df3ae1f250a1388c95e98d
karimscoding <rejaulkarim66666@gmail.com>
2024-06-11 19:56:49 +0800

    update: modified sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,39 @@
-import { siteConfig } from "@/config/site";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `${siteConfig.url}/og.png`,
+      url: "http://homefix.com.my",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://homefix.com.my/services/home-renovation",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://homefix.com.my/services/kitchen-cabinet-design",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://homefix.com.my/services/electrical-service",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://homefix.com.my/services/waterproofing",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://homefix.com.my/services/plumbing",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
